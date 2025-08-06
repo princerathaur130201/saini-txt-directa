9ef46e25b1518f946d0e13bbe9ca7cf39e925f62
princerathaur130201 <princerathaur130201@gmail.com>
2025-08-06 13:57:48 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,18 @@
+import os
+from os import environ
+
+API_ID = "20214595"
+API_HASH = "4763f66ce1a18c2dd491a5048891926c"
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
+OWNER = 6775793050
+CREDIT = Me
+
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6775793050').split(',')
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
+
+AUTH_USER = os.environ.get('AUTH_USERS', '6775793050').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
