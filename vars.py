2e82e3ae4a684a5559137401172b46118d4e49c8
princerathaur130201 <princerathaur130201@gmail.com>
2025-08-06 14:07:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ API_HASH = "4763f66ce1a18c2dd491a5048891926c"
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = 6775793050
-CREDIT = Me
+CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '6775793050').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
@@ -16,3 +16,4 @@ AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
+
